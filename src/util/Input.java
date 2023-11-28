ea612e31b04e30277247cbb462bf0739781d5dbb
JenniferLA <park.jennifer2324@gmail.com>
2023-11-27 22:20:21 -0600

    completed util package exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,2 +1,76 @@
-package util;public class Input {
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        String input = scanner.nextLine().toLowerCase();
+        return input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y");
+    }
+
+
+    public int getInt(int min, int max) {
+        int num;
+        do {
+            System.out.println("Please enter a valid integer between " + min + " and " + max + ":");
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not a number. Please enter a number: ");
+                scanner.next(); // Consume invalid input
+            }
+            num = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+        } while (num < min || num > max);
+        return num;
+    }
+
+    public int getInt() {
+        int num;
+        do {
+            System.out.println("Please enter a number: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not a number. Please enter a number: ");
+                scanner.next(); // Consume invalid input
+            }
+            num = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+        } while (num < 0);
+        return num;
+    }
+
+
+    public double getDouble(double min, double max) {
+        System.out.printf("Enter a number between %f and %f%n", min, max);
+        double aDouble = getDouble();
+        if (aDouble < min || aDouble > max) {
+            System.out.printf("Hey! That was not between %f and %f!!%n", min, max);
+            return getDouble(min, max);
+        }
+        return aDouble;
+    }
+
+    public double getDouble() {
+        double userDouble = 0;
+
+        // make sure the user doesnt screw it up
+        String s = getString();
+        try {
+            userDouble = Double.valueOf(s);
+        } catch (NumberFormatException e) {
+            // user messed up
+            System.out.print("Invalid double. Please re-enter: ");
+            userDouble = getDouble();
+        }
+
+        return userDouble;
+    }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,2 +1,35 @@
-package util;public class InputTest {
+package util;
+
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        //String getString()
+        System.out.println("Enter a string: ");
+        String userInputString = input.getString();
+        System.out.println("You entered: " + userInputString);
+
+        //boolean yesNo()
+        System.out.println("Enter 'y' or 'yes': ");
+        boolean yesNoResult = input.yesNo();
+        System.out.println("You input result: " + yesNoResult);
+
+        //int getInt(int min, int max)
+        int userInputInt = input.getInt(10, 20);
+        System.out.println("You entered: " + userInputInt);
+
+        //int getInt()
+        int userInputNum = input.getInt();
+        System.out.println("You entered: " + userInputNum);
+
+        //double getDouble(double min, double max)
+        double userInputDouble = input.getDouble(1, 19.99d);
+        System.out.println("You entered: " + userInputDouble);
+
+        //double getDouble()
+        System.out.println("Enter a floating point number: ");
+        double userInputDubNum = input.getDouble();
+        System.out.println("You entered: " + userInputDubNum);
+    }
 }
