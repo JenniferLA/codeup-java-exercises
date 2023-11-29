ddeeb602b501c76dffdcceff4b0890ee0e1a61e4
JenniferLA <park.jennifer2324@gmail.com>
2023-11-28 22:44:34 -0600

    completed Arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class ArraysExercises {
-}
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        // Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element:
+        Person[] people = new Person[3];
+        people[0] = new Person("Hank");
+        people[1] = new Person("Bobby");
+        people[2] = new Person("Peggy");
+
+
+        // Iterate through the array and print out the name of each person in the array:
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName());
+        }
+//        for (Person person : people) {
+//            System.out.println(person.getName());
+//        }
+
+        // It should accept an array of Person objects, as well as a single person object to add to the passed array.
+        Person newPerson = new Person("Luanne");
+        people = addPerson(people, newPerson);
+
+        for (Person person : people) {
+            System.out.println(person.getName());
+        }
+    }
+
+        // It should return an array whose length is 1 greater than the passed array with the passed person object at the end of the array.
+        // Create a static method named addPerson.
+        public static Person[] addPerson (Person[]people, Person newPerson){
+            // Create new array with increased length
+            Person[] newArray = new Person[people.length + 1];
+            // Copy existing elements to new array
+            System.arraycopy(people, 0, newArray, 0, people.length);
+            // Add new person at the end of new array
+            newArray[newArray.length - 1] = newPerson;
+
+            return newArray;
+        }
+    }
+
+
+
+
+
+//        public static String [] addPerson(String[] Persons, Persons newPerson){
+//            String[] updatedPersons = Arrays.copyOf(Persons,Persons.length + 1);
+//            updatedPersons[updatedPersons.length - 1] = newPerson;
+//            return updatedPersons;
+//            Persons = Person.addPerson(Persons, )
+//        }
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ServerNameGenerator {
+import java.util.Random;
+
+public class ServerNameGenerator {
+    // Create two arrays whose elements are strings:
+    // one with at least 10 adjectives
+    // another with at least 10 nouns
+    private static String[] adjectives = {"happy", "panoramic", "pale", "flippant", "endurable", "harsh", "joyous", "ubiquitous", "groovy", "dazzling"};
+
+    private static String[] nouns = {"phone", "goal", "speaker", "mode", "communication", "imagination", "inspector", "cards", "year", "guitar", "calendar"};
+
+    // Create a method that will return a random element from an array of strings
+    public static String getRandomElement(String[] array) {
+        Random random = new Random();
+        int index = random.nextInt(array.length);
+        return array[index];
+    }
+
+    // Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user.
+    public static void main(String[] args) {
+        String adjective = getRandomElement(adjectives);
+        String noun = getRandomElement(nouns);
+
+        String serverName = adjective + "-" + noun;
+
+        System.out.println("Here is your server name: ");
+        System.out.println(serverName);
+    }
 }
+
+
+
