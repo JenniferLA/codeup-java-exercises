441a3df9abe0ca7b9f1ccbafa37a5d3c87eb2b59
JenniferLA <park.jennifer2324@gmail.com>
2023-11-28 22:48:49 -0600

    completed Object Oriented Programming exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,2 +1,20 @@
-package shapes;public class Circle {
+package shapes;
+
+public class Circle {
+    //private radius field
+    private double radius;
+
+    //constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    //methods
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,17 @@
-package shapes;public class CircleApp {
+package shapes;
+
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        System.out.println("Enter the radius of the circle: ");
+        double radius = input.getDouble();
+
+        Circle circle = new Circle(radius);
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Circumference: " + circle.getCircumference());
+    }
 }
